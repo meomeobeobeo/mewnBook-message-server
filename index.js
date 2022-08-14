827dce952c16d457d6800622630e2017ff19add8
meomeobeobeo <nguyenminhducnknd@gmail.com>
2022-08-15 05:57:33 +0700

    2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const io = require('socket.io')(8900, {
+const io = require('socket.io')(process.env.PORT||8900, {
     cors: {
         origin: 'http://localhost:3000',
     },
 })
+console.log('build successfully')
 
 // {
 //   userId, socketId
